1c2919ce040a9210d472a6e4aa178cf558186f5e
Mark Layman <Mark.Layman@hotmail.com>
2020-06-08 09:40:56 -0400

    First iteration of TextMsg form.

---
+++ RPGWO Client/frmTextMsg.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace RPGWO_Client
+{
+    partial class frmTextMsg
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(481, 265);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "&Close";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.AcceptsReturn = true;
+            this.textBoxMessage.AllowDrop = true;
+            this.textBoxMessage.BackColor = System.Drawing.SystemColors.Window;
+            this.textBoxMessage.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBoxMessage.Location = new System.Drawing.Point(0, 0);
+            this.textBoxMessage.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBoxMessage.MaxLength = 0;
+            this.textBoxMessage.Multiline = true;
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.ReadOnly = true;
+            this.textBoxMessage.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBoxMessage.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxMessage.Size = new System.Drawing.Size(560, 260);
+            this.textBoxMessage.TabIndex = 1;
+            // 
+            // frmTextMsg
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(583, 320);
+            this.ControlBox = false;
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmTextMsg";
+            this.Text = "Special Message";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxMessage;
+    }
+}
